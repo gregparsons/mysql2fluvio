15e480ad2abdbe7bb5c6cdb428ca22e2d6e70a2b
Greg <greg.parsons@inclimesolutions.com>
2022-10-31 16:04:52 -0700

    init

--- fluvio_start.sh
+++ fluvio_start.sh
@@ -1,8 +1,8 @@
 cd fluvio
-# make clean
-# make -C k8-util/helm clean
-# export FLUVIO_BUILD_LLD=/usr/local/bin/lld
-# make build_k8_image
+make clean
+make -C k8-util/helm clean
+export FLUVIO_BUILD_LLD=/usr/local/bin/lld
+make build_k8_image
 # not needed with my hack to cli source
 # cargo run --package fluvio-cli -- cluster delete
 cargo run --package fluvio-cli -- cluster start --proxy-addr localhost --develop
